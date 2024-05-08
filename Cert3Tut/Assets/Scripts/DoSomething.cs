f58e49916ea3522dc27ff4df014cae385ee57b27
CameronJMurphy <cameronmurphyofficial@gmail.com>
2024-05-08 18:43:36 +1000

    best changes oce

--- Cert3Tut/Assets/Scripts/DoSomething.cs
+++ Cert3Tut/Assets/Scripts/DoSomething.cs
@@ -8,7 +8,7 @@ public class DoSomething : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        value *= 2;
     }
 
     // Update is called once per frame
